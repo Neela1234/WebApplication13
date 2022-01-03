5f3b3e50c0aa934700c870d45bedbb3918c9ec77
home <home@WINDOWS-PQ21EPG>
2022-01-03 11:02:40 +0530

    Add project files.

---
+++ WebApplication13/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication13.Models;
+
+namespace WebApplication13.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Account> accounts = new List<Account>();
+            accounts.Add(new Account() { ID = 123, Name = "neela", Address = "khammam" });
+            accounts.Add(new Account() { ID = 345, Name = "AMMU", Address = "hyderabad" });
+            accounts.Add(new Account() { ID = 456, Name = "kousik", Address = "vijayawada" });
+            ViewData["accounts"] = accounts;
+
+
+            return View();
+        }
+    }
+}
